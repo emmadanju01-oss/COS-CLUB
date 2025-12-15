aafa11cb8dddea79d701fffb5d6adc23cddfd2df
emmadanju01-oss <emmadanju01@gmail.com>
2025-12-15 22:41:43 +0100

    Add Hausa dictionary to language translator

--- DICT.py
+++ DICT.py
@@ -46,6 +46,27 @@ igbo = {
     "thank you":"daalu"
 }
 
+Hausa ={
+    "water" : "ruwa",
+    "food" :"abinci",
+    "house": "gida",
+    "person": "mutum",
+    "child" : "yaro",
+    "girl" : "yarinya",
+    "money" : "kudi",
+    "work" : "aiki",
+    "road" : "hanaya",
+     "book" : "littafi",
+    "market" : "kasuwa",
+    "to see" : "gani",
+    "to listen" : "saurara",
+    "speech/talk" : "magana",
+    "time" : "lokaci",
+    "day/sun" : "rana",
+    "night" : "dare",
+    "walking/journey" : "tafiya",
+    "heart/mind" : "zuciya"
+}
 
 
 
